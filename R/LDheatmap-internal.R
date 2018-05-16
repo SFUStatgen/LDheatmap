5b1adefcad8044c72b58bd645a2073e9911c4023
Gloria Yang <gloriayang@d207-023-195-186.wireless.sfu.ca>
2018-05-16 10:30:33 -0700

    documenting internal function

---
+++ R/LDheatmap-internal.R
@@ -0,0 +1,13 @@
+#' @name LDheatmap-internal
+#' @title Internal LDheatmap functions and data files
+#' @aliases preDrawDetails.ldheatmap
+#' postDrawDetails.ldheatmap
+#' preDrawDetails.symbols
+#' postDrawDetails.symbols
+#' constructVP
+#' drawLDheatmapGrob
+#' moveTitles
+#' @description Internal LDheatmap functions.
+#' @details These are not to be called by the user or are undocumented.
+#' @keywords internal
+NULL
